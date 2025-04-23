da9bdcadf167ae6227d169ab26d49651887bc09d
delmo19 <1912261h@umich.mx>
2025-04-22 21:39:20 -0600

    Cambios

--- codigo-fuente/diftiempo.c
+++ codigo-fuente/diftiempo.c
@@ -1,6 +1,7 @@
-#include "tiempo.h"
+#include "diftiempo.h"
 
 long diferencia(struct timeval *inicio, struct timeval *final) {
-    long micros = final->tv_usec - inicio->tv_usec;
-    return micros;
+    long secs  = final->tv_sec  - inicio->tv_sec;
+    long usecs = (final->tv_usec - inicio->tv_usec) / 1000;
+    return secs * 1000000L + usecs;
 }
--- codigo-fuente/main.c
+++ codigo-fuente/main.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/time.h>
 #include <time.h>
 #include <locale.h>
-#include "util.h"
 #include "diftiempo.h"
 #include "burbuja.h"
 #include "insercion.h"
 
 int main() {
-    int i, opt, opc, bucle = 1;
-    int *arreglo;
-    int *arr_burbuja, *arr_insercion;
-    long tiempo;
-    unsigned tam, in, fin;
-    struct timeval inicio, final;
-
     setlocale(LC_NUMERIC, "");
+    srand((unsigned)time(NULL));
+
+    unsigned tam, in, fin;
+    int i, opt, opc, bucle = 1;
+    int *arreglo, *arr_burbuja, *arr_insercion;
+    long tiempo;
+    struct timeval inicio, final;
 
     while (bucle) {
 
@@ -26,23 +24,21 @@ int main() {
         do {
             printf("Ingrese el tamaño del arreglo: ");
             scanf("%u", &tam);
-            if (tam <= 0) {
+            if (scanf("%u", &tam) != 1 || tam == 0) {
                 printf("Tamaño inválido.\n");
             } else break;
         } while (1);
 
         do {
             printf("Ingrese el rango inferior: ");
-            scanf("%u", &in);
-            if (in <= 0) {
+            if (scanf("%u", &in) != 1) {
                 printf("Rango inferior inválido.\n");
             } else break;
         } while (1);
 
         do {
             printf("Ingrese el rango superior: ");
-            scanf("%u", &fin);
-            if (fin <= 0 || fin < in) {
+            if (scanf("%u", &fin) != 1 || fin < in) {
                 printf("Rango superior inválido.");
             } else break;
         } while (1);
@@ -60,7 +56,6 @@ int main() {
     
         // Paso 2, generar números aleatorios.
 
-        srand(time(NULL));
         for(i = 0; i < tam; i++) {
             arreglo[i] = in + rand() % (fin - in + 1);
         }
@@ -71,25 +66,19 @@ int main() {
         memcpy(arr_insercion, arreglo, tam * sizeof(int));
     
         // Burbuja.
-        imprimir(arr_burbuja, tam);
 
         gettimeofday(&inicio, NULL);
         burbuja(arr_burbuja, tam);
         gettimeofday(&final, NULL);
         tiempo = diferencia(&inicio, &final);
-
-        imprimir(arr_burbuja, tam);
         printf("Burbuja: %'ld us\n", tiempo);
     
         // Inserción.
-        imprimir(arr_insercion, tam);
 
         gettimeofday(&inicio, NULL);
         insercion(arr_insercion, tam);
         gettimeofday(&final, NULL);
         tiempo = diferencia(&inicio, &final);
-
-        imprimir(arr_insercion, tam);
         printf("Inserción: %'ld us\n", tiempo);
     
         printf("Si deseas continuar, presiona 1.\n");
